542a914468dccaea06196b94dd1396105b1920d1
nextlevelstudio <kontakt@nextlevelstudio.pl>
2021-02-20 23:24:25 +0100

    update

--- js/main.js
+++ js/main.js
@@ -2,19 +2,21 @@
 $('.black .overlay').click(function(){
     console.log(this);
     this.style.transition = 'all 2s ease';
-    this.style.position = 'fixed';
+    
     this.style.height = '100vh';
     this.style.width = '50vw';
     this.style.opacity = '1';
+    this.style.position = 'fixed';
     this.style.background = 'rgba(0,0,0,1)';
     this.style.zIndex = '999';
 });
 $('.blue .overlay').click(function(){
     console.log(this);
     this.style.transition = 'all 2s ease';
-    this.style.position = 'fixed';
+    
     this.style.height = '100vh';
     this.style.width = '50vw';
+    this.style.position = 'fixed';
     this.style.opacity = '1';
     this.style.background = 'rgba(0,203,255,1)';
     this.style.zIndex = '999';
@@ -22,10 +24,12 @@ $('.blue .overlay').click(function(){
 $('.white .overlay').click(function(){
     console.log(this);
     this.style.transition = 'all 2s ease';
-    this.style.position = 'fixed';
+    
     this.style.height = '100vh';
     this.style.width = '50vw';
     this.style.opacity = '1';
+    this.style.position = 'fixed';
     this.style.background = 'rgba(255,255,255,1)';
     this.style.zIndex = '999';
 });
+
